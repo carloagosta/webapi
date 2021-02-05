d63c10f9699a753e042aa2446fc954c347c383c9
carloagosta <78606196+carloagosta@users.noreply.github.com>
2021-02-05 16:06:53 +0100

    Add files via upload

---
+++ WebApplication1/Resources/Price.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Resources
+{
+    public class Point
+    {
+        public DateTime Date;
+        public decimal Price;
+
+        public Point(DateTime date, decimal price)
+        {
+            Date = date;
+            Price = price;
+        }
+    }
+}
---
+++ WebApplication1/Resources/PriceResource.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace WebApplication1.Resources
+{
+    public class PriceResource
+    {
+        private static string _ResourceAdress = "https://datahub.io/core/oil-prices/r/brent-daily.json";
+
+        private static List<Point> LoadListFromWeb()
+        {
+            using(WebClient wc = new WebClient())
+            {
+                string json = string.Empty;
+
+                try
+                {
+                    json = wc.DownloadString(_ResourceAdress);
+
+                    List<Point> pnt = JsonConvert.DeserializeObject<List<Point>>(json);
+
+                    return pnt;
+                }
+                catch(Exception ex)
+                {
+                    return null;
+                }
+            }
+        }
+        
+        public static List<Point> GetPricesList()
+        {
+            List<Point> lst = LoadListFromWeb();
+
+            return lst != null ? LoadListFromWeb().ToList() : new List<Point>();
+        }
+
+        public static List<Point> GetPricesListRange(DateTime from, DateTime to)
+        {
+            List<Point> lst = LoadListFromWeb();
+
+            return lst != null ? lst.Where(o => o.Date >= from && o.Date <= to).ToList() : new List<Point>();
+        }
+
+        //Se volessimo scaricare i dati una sola volta perchè ipotizziamo che siano costanti
+        //è possibile avvalersi dell'istanza statica _PricesList
+        //modificando i metodi Get utilizzando LoadList invece di LoadListFromWeb
+        private static List<Point> _PricesList = null;
+
+        private static List<Point> LoadList()
+        {
+            if (_PricesList == null)
+            {
+                _PricesList = LoadListFromWeb();
+            }
+
+            return _PricesList;
+        }
+    }
+}
